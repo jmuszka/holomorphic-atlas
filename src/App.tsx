173beb1be85f726600ce79588b7744ae37920ced
Josh Muszka <jmuszka@uwo.ca>
2026-02-06 17:37:10 -0500

    chore: moved glsl code from inline string to discrete files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useRef, useEffect, useState } from 'react';
+import vertexShaderSource from './shaders/vertex.glsl?url'
+import fragmentShaderSource from './shaders/fragment.glsl?url'
 
 function App() {
   const [fidelity, setFidelity] = useState(1.0);
   const canvasRef = useRef(null);
 
-  // OpenGL rendering
-  useEffect(() => {
+  const render = async () => {
     const canvas = canvasRef.current;
     const gl = canvas.getContext('webgl');
 
@@ -34,42 +35,12 @@ function App() {
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
-    let vsSource = 
-      'attribute vec3 coordinates;' +
-				
-      'void main(void) {' +
-        ' gl_Position = vec4(coordinates, 1.0);' +
-      '}';
+    const vsSource = await fetch(vertexShaderSource).then(res => res.text())
     let vs = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vs, vsSource);
     gl.compileShader(vs);
 
-    let fsSource =
-      'precision highp float;' +
-      'uniform vec2 u_resolution;' +
-
-      'void main(void) {' +
-        'vec4 coords = vec4(2.0*gl_FragCoord.x/u_resolution.x - 1.0, 2.0*gl_FragCoord.y/u_resolution.y - 1.0, 0.0, 1.0);' +
-        `coords = vec4(coords.x*(${window.innerWidth/window.innerHeight}), coords.yzw);` +
-
-        'float x0 = coords.x; float y0 = coords.y;' +
-        'float x = 0.0; float y = 0.0; float iteration = 0.0;' +
-
-        'for (int i = 0; i < 10000; i++) {' +
-          'if (x*x + y*y > 4.0) break;' +
-          'float xtemp = x*x - y*y + x0;' +
-          'y = 2.0*x*y + y0;' +
-          'x = xtemp;' +
-          'iteration++;' +
-        '}' +
-
-        `vec4 color;` +
-        'if (iteration >= 10000.0) color = vec4(0.0, 0.0, 0.0, 1.0);' +
-        'else if (iteration >= 50.0) color = vec4(1.0, 1.0, 0.0, 1.0);' +
-        'else color = vec4(0.0, 0.0, 1.0, 1.0);' +
-
-        'gl_FragColor = color;' +
-      '}';
+    let fsSource = await fetch(fragmentShaderSource).then(res => res.text())
     let fs = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fs, fsSource);
     gl.compileShader(fs);
@@ -89,10 +60,18 @@ function App() {
     let uResLoc = gl.getUniformLocation(shaderProgram, 'u_resolution');
     gl.uniform2f(uResLoc, canvas.width, canvas.height);
 
+    let uRatioLoc = gl.getUniformLocation(shaderProgram, 'u_ratio');
+    gl.uniform1f(uRatioLoc, window.innerWidth/window.innerHeight);
+
     gl.clearColor(0.5, 0.5, 1.0, 0.9);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+  }
+
+  // OpenGL rendering
+  useEffect(() => {
+    render();
   }, [])
 
   return (
